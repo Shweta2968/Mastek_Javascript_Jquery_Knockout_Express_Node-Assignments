cd1d88459537a391057569e0316292ad78f72ed7
Shweta Patil <89509921+Shweta2968@users.noreply.github.com>
2021-10-06 18:19:35 +0530

    Add files via upload

---
+++ Assignment5_JSExpress_Form/js/main.js
@@ -0,0 +1,14 @@
+
+// function Submit() {
+//     var firstName = document.getElementById("fname").value;
+//     var lastName = document.getElementById("lname").value;
+//     if (firstName == "") {
+//         alert("");
+//         return false;
+//     }
+//     if (lastName == "") {
+//         alert("");
+//         return false;
+//     }
+
+// }
