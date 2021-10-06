9b92ef78e3d4c482d2bfbf4fa1e4abd4f2d210fb
Shweta Patil <89509921+Shweta2968@users.noreply.github.com>
2021-10-06 18:27:29 +0530

    Add files via upload

---
+++ Assignment7_Javascript_Hobbies/javascript/js/main.js
@@ -0,0 +1,21 @@
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+  
+  
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
